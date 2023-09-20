f3680e0d3b79db73149e13d2e5e1de2e28f31a1b
anna.torgova <anna.torgova@lenvendo.ru>
2023-09-20 22:20:58 +0300

    Правка в ожидаемом результате поиска

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from selene import be, have
 def test_result_is_found():
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
-    browser.element('[id="search"]').should(have.text('browser tests in Python'))
+    browser.element('[id="search"]').should(have.text('browser tests in'))
 
 
 def test_result_not_found():
